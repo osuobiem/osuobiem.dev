6db5b8d834bfb8354a4ee82254cf586ebb18104f
Gabriel Osuobiem <osuobiem@gmail.com>
2020-12-21 23:32:20 +0100

    Add portfolio table and migration

---
+++ database/migrations/2020_12_21_222927_create_portfolio_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePortfolioTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('portfolio', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('link');
+            $table->string('photo');
+            $table->text('details');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('portfolio');
+    }
+}
